79ae73a0eedd5b1455d00030e09f7eeed4cb78a7
simon <simonnouwens@gmail.com>
2020-09-30 09:16:43 +0200

    small bugfixes

--- talks/views.py
+++ talks/views.py
@@ -44,6 +44,8 @@ class DetailView(generic.DetailView):
             audio = self.get_object().audio_cleaned
         elif self.get_object().audio_original is not None:
             audio = self.get_object().audio_original
+        if not hasattr(audio, 'url'):
+            audio = False
         context['audio'] = audio
         return context
 
