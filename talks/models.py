ab9e8527db691dfa57d1d7e4946c86a60371a3a6
Alex Morega <alex@grep.ro>
2022-01-07 20:18:41 +0200

    Playlist view page

--- talks/models.py
+++ talks/models.py
@@ -66,3 +66,6 @@ class Playlist(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('playlist_view', kwargs={'pk': self.id})
--- talks/urls.py
+++ talks/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('', views.IndexView.as_view(), name='talk_index'),
     path('talk/<int:pk>', views.DetailView.as_view(), name='talk_view'),
     path('talk/<int:pk>/edit', views.UpdateView.as_view(), name='talk_edit'),
+    path('playlist/<int:pk>', views.playlist_view, name='playlist_view'),
     path('accounts/profile/', views.profile_view, name='profile_view'),
     path('contact', views.contact, name='contact')
 
