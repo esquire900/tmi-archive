faac4127b14eb2604f75fab53dc98bfe715541ec
simon <simon@simonnouwens.nl>
2022-01-20 16:51:12 +0100

    fix origin

--- talks/views.py
+++ talks/views.py
@@ -64,7 +64,7 @@ class NewDetailView(DetailView):
 def talk_transcription(request, pk):
     talk = get_object_or_404(Talk, pk=pk)
     response = HttpResponse(talk.transcription, content_type="text/plain", charset='utf-8')
-    response["Access-Control-Allow-Origin"] = "https://tmi-archive.com"
+    response["Access-Control-Allow-Origin"] = "*"
     return response
 
 
