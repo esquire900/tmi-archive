c09f5b900e67f388b15bcc8ce112141e4d5bb7dd
Alex Morega <alex@grep.ro>
2022-02-25 13:48:51 +0200

    Sort talks in editor and view them in order

--- talks/forms.py
+++ talks/forms.py
@@ -2,7 +2,6 @@ from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit
 from django import forms
 from tinymce.widgets import TinyMCE
-from django_select2.forms import ModelSelect2MultipleWidget
 from .models import Talk, Playlist
 
 
@@ -18,17 +17,24 @@ class TalkForm(forms.ModelForm):
         fields = ['title', 'description', 'transcription']
 
 
+class SortableTalksField(forms.ModelMultipleChoiceField):
+    def clean(self, value):
+        # First, call the default implementation, to get a queryset of talks.
+        queryset = super().clean(value)
+        # If no error was raised, reconstruct the order from the form value.
+        ordering = {pk: index for index, pk in enumerate(value)}
+        # Sort the queryset and return a list of talks.
+        return sorted(list(queryset), key=lambda item: ordering[str(item.pk)])
+
 
 class PlaylistForm(forms.ModelForm):
     description = forms.CharField(widget=TinyMCE(attrs={'cols': 80, 'rows': 10}))
     first_recording_date = forms.DateInput(
         # =
     )
-    talks = forms.ModelMultipleChoiceField(
+    talks = SortableTalksField(
         queryset=Talk.objects.all(),
-        widget=ModelSelect2MultipleWidget(
-            search_fields=['title__icontains']
-        ),
+        widget=forms.CheckboxSelectMultiple(),
         help_text='Which talks to include in the playlist. Select talks by searching in the field above. Talks are not sortable at the moment.'
     )
 
